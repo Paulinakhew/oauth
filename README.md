b77933d4a15dd4ccc88b721e8a3fe2b96a38ff14
Paulina Khew <paulina.khew@shopify.com>
2021-02-03 18:16:53 -0500

    Add index.php and README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # oauth
+The PHP script is from [here](https://www.oauth.com/oauth2-servers/accessing-data/create-an-application/). 
+
+## Setup
+Go to https://github.com/settings/developers, create a new OAuth App, and put your client ID and client secret in an .env file.
+
+Contents of your env file should be:
+```
+GITHUB_CLIENT_ID=
+GITHUB_CLIENT_SECRET=
+```
+
+Finally, you can run this command from within this directory
+```
+php -S localhost:8000
+```
---
+++ index.php
@@ -0,0 +1,127 @@
+<?php
+// Fill these out with the values you got from Github
+$githubClientID = $_SERVER["GITHUB_CLIENT_ID"];
+$githubClientSecret = $_SERVER["GITHUB_CLIENT_SECRET"];
+
+// This is the URL we'll send the user to first to get their authorization
+$authorizeURL = 'https://github.com/login/oauth/authorize';
+
+// This is the endpoint our server will request an access token from
+$tokenURL = 'https://github.com/login/oauth/access_token';
+
+// This is the Github base URL we can use to make authenticated API requests
+$apiURLBase = 'https://api.github.com/';
+
+// The URL for this script, used as the redirect URL
+$baseURL = 'http://localhost:8000/callback';
+
+// Start a session so we have a place to store things between redirects
+session_start();
+
+
+// Start the login process by sending the user
+// to Github's authorization page
+if(isset($_GET['action']) && $_GET['action'] == 'login') {
+  unset($_SESSION['access_token']);
+
+  // Generate a random hash and store in the session
+  $_SESSION['state'] = bin2hex(random_bytes(16));
+
+  $params = array(
+    'response_type' => 'code',
+    'client_id' => $githubClientID,
+    'redirect_uri' => $baseURL,
+    'scope' => 'user public_repo',
+    'state' => $_SESSION['state']
+  );
+
+  // Redirect the user to Github's authorization page
+  header('Location: '.$authorizeURL.'?'.http_build_query($params));
+  die();
+}
+
+
+if(isset($_GET['action']) && $_GET['action'] == 'logout') {
+  unset($_SESSION['access_token']);
+  header('Location: '.$baseURL);
+  die();
+}
+
+// When Github redirects the user back here,
+// there will be a "code" and "state" parameter in the query string
+if(isset($_GET['code'])) {
+  // Verify the state matches our stored state
+  if(!isset($_GET['state'])
+    || $_SESSION['state'] != $_GET['state']) {
+
+    header('Location: ' . $baseURL . '?error=invalid_state');
+    die();
+  }
+
+  // Exchange the auth code for an access token
+  $token = apiRequest($tokenURL, array(
+    'grant_type' => 'authorization_code',
+    'client_id' => $githubClientID,
+    'client_secret' => $githubClientSecret,
+    'redirect_uri' => $baseURL,
+    'code' => $_GET['code']
+  ));
+  $_SESSION['access_token'] = $token['access_token'];
+
+  header('Location: ' . $baseURL);
+  die();
+}
+
+
+if(isset($_GET['action']) && $_GET['action'] == 'repos') {
+  // Find all repos created by the authenticated user
+  $repos = apiRequest($apiURLBase.'user/repos?'.http_build_query([
+    'sort' => 'created',
+    'direction' => 'desc'
+  ]));
+
+  echo '<ul>';
+  foreach($repos as $repo) {
+    echo '<li><a href="' . $repo['html_url'] . '">'
+      . $repo['name'] . '</a></li>';
+  }
+  echo '</ul>';
+}
+
+// If there is an access token in the session
+// the user is already logged in
+if(!isset($_GET['action'])) {
+  if(!empty($_SESSION['access_token'])) {
+    echo '<h3>Logged In</h3>';
+    echo '<p><a href="?action=repos">View Repos</a></p>';
+    echo '<p><a href="?action=logout">Log Out</a></p>';
+  } else {
+    echo '<h3>Not logged in</h3>';
+    echo '<p><a href="?action=login">Log In</a></p>';
+  }
+  die();
+}
+
+
+// This helper function will make API requests to GitHub, setting
+// the appropriate headers GitHub expects, and decoding the JSON response
+function apiRequest($url, $post=FALSE, $headers=array()) {
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+  if($post)
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+
+  $headers = [
+    'Accept: application/vnd.github.v3+json, application/json',
+    'User-Agent: https://example-app.com/'
+  ];
+
+  if(isset($_SESSION['access_token']))
+    $headers[] = 'Authorization: Bearer ' . $_SESSION['access_token'];
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $response = curl_exec($ch);
+  return json_decode($response, true);
+}
